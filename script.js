84120710066c6719849d421a0aeb045e3ff44784
kartik132000 <sujeetkumarsen37@gmail.com>
2025-02-21 17:29:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
-// Animal Class
 class Animal {
-    constructor(species) {
-        this._species = species;
-    }
+  constructor(species) {
+    this._species = species;
+  }
 
-    get species() {
-        return this._species;
-    }
+  get species() {
+    return this._species;
+  }
 
-    makeSound() {
-        console.log(`The ${this._species} makes a sound`);
-    }
+  makeSound() {
+    console.log(The ${this._species} makes a sound);
+  }
 }
 
-// Cat Class (inherits from Animal)
-class Cat extends Animal {
-    purr() {
-        console.log("purr");
-    }
-}
-
-// Dog Class (inherits from Animal)
 class Dog extends Animal {
-    bark() {
-        console.log("woof");
-    }
+  bark() {
+    console.log("woof");
+  }
 }
 
-// Example Usage
-const myCat = new Cat("Siamese");
-myCat.makeSound(); // Output: The Siamese makes a sound
-myCat.purr();      // Output: purr
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
 
-const myDog = new Dog("Golden Retriever");
-myDog.makeSound(); // Output: The Golden Retriever makes a sound
-myDog.bark();      // Output: woof
-
-// Do not change the code below
+// Do not change the code below this line
 window.Animal = Animal;
-window.Cat = Cat;
 window.Dog = Dog;
+window.Cat = Cat;
